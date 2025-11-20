f334264dd5c7c7a731b099885e31ef4bca27390a
kadri <kadriyaakoub@gmail.com>
2025-11-19 19:46:55 -0800

    add api-gateway service: fix aggregation controller

--- API Gateway/api-gateway/src/main/java/com/kadri/api_gateway/controller/OrderAggregationController.java
+++ API Gateway/api-gateway/src/main/java/com/kadri/api_gateway/controller/OrderAggregationController.java
@@ -8,6 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -27,23 +28,47 @@ public class OrderAggregationController {
                 .retrieve()
                 .bodyToMono(Map.class);
 
-        Mono<Map> productMono = webClient.get()
-                .uri("http://product-service/api/v1/products/by-order/{id}", id)
-                .retrieve()
-                .bodyToMono(Map.class);
+        return orderMono.flatMap(order -> {
+            Long userId = ((Number)order.get("userId")).longValue();
+            List<Integer> productIds = extractProductsIds(order);
 
-        Mono<Map> userMono = webClient.get()
-                .uri("http://user-service/api/v1/users/by-order/{id}", id)
-                .retrieve()
-                .bodyToMono(Map.class);
+            Mono<Map> userMono = webClient.get()
+                    .uri("http://user-service/api/v1/users/{userId}", userId)
+                    .retrieve()
+                    .bodyToMono(Map.class);
 
-        return Mono.zip(orderMono, productMono, userMono)
-                .map(tuple -> {
-                    Map<String, Object> result = new HashMap<>();
-                    result.put("order", tuple.getT1());
-                    result.put("product", tuple.getT2());
-                    result.put("user", tuple.getT3());
-                    return result;
-                });
+            Mono<List> productMono = webClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .scheme("http")
+                            .host("product-service")
+                            .path("/products/by-ids")
+                            .queryParam("ids", productIds)
+                            .build()
+                    )
+                    .retrieve()
+                    .bodyToMono(List.class);
+
+            return Mono.zip(userMono, productMono)
+                    .map(tuple -> {
+                        Map<String, Object> result = new HashMap<>();
+                        result.put("order", order);
+                        result.put("user", tuple.getT1());
+                        result.put("products", tuple.getT2());
+                        return result;
+                    });
+        });
+
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<Long> extractProductsIds(Map<String, Object> order) {
+        List<Map<String, Object>> items =
+                (List<Map<String, Object>>) order.get("items");
+
+        if (items == null) return List.of();
+
+        return items.stream()
+                .map(item -> ((Number) item.get("productId")).longValue())
+                .toList();
     }
 }
